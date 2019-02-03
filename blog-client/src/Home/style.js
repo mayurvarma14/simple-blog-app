ab0652fff54894ac7c46ebca5846310e68dab518
Mayur Varma <mayurvarma14@gmail.com>
2019-02-03 15:29:11 +0530

    Added blog client app

---
+++ blog-client/src/Home/style.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  .ant-card {
+    height: calc(100vh - 104px);
+  }
+  .ant-card-body {
+    padding: 0 !important;
+  }
+`;
